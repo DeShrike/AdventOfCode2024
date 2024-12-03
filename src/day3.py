6b6047b3e7d6c5ef232f5513d3a9535e0b2055b1
Shrike <de_shrike@hotmail.com>
2024-12-03 18:29:33 +0100

    Day 3 (WIP)

---
+++ src/day3.py
@@ -0,0 +1,94 @@
+from aoc import Aoc
+import itertools
+import math
+import re
+import sys
+
+# Day 3
+# https://adventofcode.com/2024
+
+class Day3Solution(Aoc):
+
+   def Run(self):
+      self.StartDay(3, "Mull It Over")
+      self.ReadInput()
+      self.PartA()
+      self.PartB()
+
+   def Test(self):
+      self.StartDay(3)
+
+      goal = self.TestDataA()
+      self.PartA()
+      self.Assert(self.GetAnswerA(), goal)
+
+      goal = self.TestDataB()
+      self.PartB()
+      self.Assert(self.GetAnswerB(), goal)
+
+   def TestDataA(self):
+      self.inputdata.clear()
+      testdata = \
+      """
+      xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+      """
+      self.inputdata = [line.strip() for line in testdata.strip().split("\n")]
+      return 161
+
+   def TestDataB(self):
+      self.inputdata.clear()
+      # self.TestDataA()    # If test data is same as test data for part A
+      testdata = \
+      """
+      1000
+      2000
+      3000
+      """
+      self.inputdata = [line.strip() for line in testdata.strip().split("\n")]
+      return None
+
+   def ParseInput(self):
+      # rx = re.compile("^(?P<from>[A-Z0-9]{3}) = \((?P<left>[A-Z0-9]{3}), (?P<right>[A-Z0-9]{3})\)$")
+      # match = rx.search(line)
+      # pos = match["from"]
+
+      data = []
+      for line in self.inputdata:
+         data.append(line)
+
+      return data
+
+   def PartA(self):
+      self.StartPartA()
+
+      rx = re.compile("mul\((?P<num1>[0-9]{1,3}),(?P<num2>[0-9]{1,3})\)")
+
+      data = self.ParseInput()
+      answer = 0
+      for line in data:
+         matches = rx.findall(line)
+         for n1, n2 in matches:
+            answer += int(n1) * int(n2)
+
+      self.ShowAnswer(answer)
+
+   def PartB(self):
+      self.StartPartB()
+
+      data = self.ParseInput()
+      answer = None
+
+      # Add solution here
+
+      self.ShowAnswer(answer)
+
+
+if __name__ == "__main__":
+   solution = Day3Solution()
+   if len(sys.argv) >= 2 and sys.argv[1] == "test":
+      solution.Test()
+   else:
+      solution.Run()
+
+# Template Version 1.5
+
