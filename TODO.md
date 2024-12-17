a501c4f10ddad01a937a37e3c6baf7f299e24b7c
Shrike <de_shrike@hotmail.com>
2024-12-17 21:06:24 +0100

    Day 17 (WIP)

--- TODO.md
+++ TODO.md
@@ -7,3 +7,6 @@ Day 11 part 2.
 Day 12 part 2.
 
 Day 14 part 2 is too slow.
+Day 16 part 1 and 2.
+
+Day 17 part 2.
--- src/day17.py
+++ src/day17.py
@@ -41,15 +41,16 @@ class Day17Solution(Aoc):
 
    def TestDataB(self):
       self.inputdata.clear()
-      # self.TestDataA()    # If test data is same as test data for part A
       testdata = \
       """
-      1000
-      2000
-      3000
+      Register A: 2024
+      Register B: 0
+      Register C: 0
+
+      Program: 0,3,5,4,3,0
       """
       self.inputdata = [line.strip() for line in testdata.strip().split("\n")]
-      return None
+      return 117440
 
    def ParseInput(self):
       # rx = re.compile("^(?P<from>[A-Z0-9]{3}) = \((?P<left>[A-Z0-9]{3}), (?P<right>[A-Z0-9]{3})\)$")
@@ -58,6 +59,7 @@ class Day17Solution(Aoc):
 
       a = b = c = None
       prog = None
+      raw = None
       for line in self.inputdata:
          if line[:12] == "Register A: ":
             a = int(line[12:])
@@ -66,21 +68,18 @@ class Day17Solution(Aoc):
          elif line[:12] == "Register C: ":
             c = int(line[12:])
          elif line[:9] == "Program: ":
-            prog = [int(i) for i in line[9:].split(",")]
+            raw = line[9:]
+            prog = [int(i) for i in raw.split(",")]
 
-      return a, b, c, prog
+      return a, b, c, prog, raw
 
-   def PartA(self):
-      self.StartPartA()
-
+   def Execute(self, prog, aa) -> str:
       opcodes = ["adv", "bxl", "bst", "jnz", "bxc", "out", "bdv", "cdv"]
-      a, b, c, prog = self.ParseInput()
-      print(a)
-      print(b)
-      print(c)
-      print(prog)
       answer = ""
       ip = 0
+      a = aa
+      b = c = 0
+      result = ""
       while True:
          if ip >= len(prog):
             break
@@ -94,6 +93,46 @@ class Day17Solution(Aoc):
             operand = c
 
          #print(f"IP: {ip} {opcode} Literal: {literal} Operand: {operand}")
+         #print(f"A: {a}  B: {b}   C: {c}")
+         if opcode == opcodes[0]:
+            a = a // (2 ** operand)
+            ip += 2
+         elif opcode == opcodes[1]:
+            b = b ^ literal
+            ip += 2
+         elif opcode == opcodes[2]:
+            b = (operand % 8) & 0x07
+            ip += 2
+         elif opcode == opcodes[3]:
+            if a != 0:
+               ip = literal
+            else:
+               ip += 2
+         elif opcode == opcodes[4]:
+            b = b ^ c
+            ip += 2
+         elif opcode == opcodes[5]:
+            if result != "":
+               result += ","
+            result += str(operand % 8)
+            ip += 2
+         elif opcode == opcodes[6]:
+            b = a // (2 ** operand)
+            ip += 2
+         elif opcode == opcodes[7]:
+            c = a // (2 ** operand)
+            ip += 2
+      return result
+
+   def ExecuteB(self, prog):
+      opcodes = ["adv", "bxl", "bst", "jnz", "bxc", "out", "bdv", "cdv"]
+      need = list(reversed(prog))
+      ip = len(prog) - 2
+      a = b = c = 0
+      while True:
+         opcode = opcodes[prog[ip]]
+         operand = literal = prog[ip + 1]
+
 
          if opcode == opcodes[0]:
             a = a // (2 ** operand)
@@ -113,9 +152,9 @@ class Day17Solution(Aoc):
             b = b ^ c
             ip += 2
          elif opcode == opcodes[5]:
-            if answer != "":
-               answer += ","
-            answer += str(operand % 8)
+            if result != "":
+               result += ","
+            result += str(operand % 8)
             ip += 2
          elif opcode == opcodes[6]:
             b = a // (2 ** operand)
@@ -123,17 +162,38 @@ class Day17Solution(Aoc):
          elif opcode == opcodes[7]:
             c = a // (2 ** operand)
             ip += 2
+         
+      pass
+
+   def PartA(self):
+      self.StartPartA()
+
+      a, b, c, prog, raw = self.ParseInput()
+      answer = self.Execute(prog, a)
 
       self.ShowAnswer(answer)
 
    def PartB(self):
       self.StartPartB()
 
-      a, b, c, prog = self.ParseInput()
+      opcodes = ["adv", "bxl", "bst", "jnz", "bxc", "out", "bdv", "cdv"]
+      a, b, c, prog, raw = self.ParseInput()
+      aa = 0
+      print(prog)
+      print(raw)
+      while True:
+         aa += 5
+         print(f"{aa}", end="\r")
+         result = self.Execute(prog, aa)
+         if result == raw:
+            answer = aa
+            break
+         #if aa % 1000000000 == 0:
+         #   print(result)
+         print(f"{aa} -> {result}")
+         #aaa = input()
+         break
       answer = None
-
-      # Add solution here
-
       self.ShowAnswer(answer)
 
 
